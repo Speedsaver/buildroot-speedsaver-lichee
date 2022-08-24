f39b908e7885a343e839b774b45e05f146b06962
Xogium <contact@xogium.me>
2022-08-25 00:43:36 +0200

    Revert "board/speedsaver: disable v4l subsystem and enable usb gadget serial."
    This is so broken right now that it isn't workth the hassle. Suspecting a SoC defect or a driver problem, the latter being unlikely given this same driver works on orange pi zero without a problem.
    
    This reverts commit dd2ea20d89a11482c770aca2992ea3f6c03b874c.

--- board/speedsaver/utilities/boot.cmd
+++ board/speedsaver/utilities/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 panic=5 console=ttyGS0,115200 rootwait root=/dev/mmcblk0p3 earlyprintk rw
+setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait root=/dev/mmcblk0p3 earlyprintk rw
 load mmc 0:2 0x41000000 zImage
 load mmc 0:2 0x41800000 sun8i-v3s-licheepi-zero.dtb
 bootz 0x41000000 - 0x41800000
