8222cf04fd794914c0729c2039f155f50c8875bc
Jookia <contact@jookia.org>
2022-12-05 08:03:34 +1100

    Bump navit

--- packages/navit/navit.mk
+++ packages/navit/navit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NAVIT_VERSION = aa7181cd5dbccabd527f98bdf244125897be5f9b
+NAVIT_VERSION = 8b5c241c8bb4b0a56d0a0df4b57c3eb89d958f9c
 NAVIT_SITE = $(call github,speedsaver,navit,$(NAVIT_VERSION))
 NAVIT_LICENSE = GPL-2
 NAVIT_DEPENDENCIES = host-pkgconf arduipi-oled libglib2 gpsd
